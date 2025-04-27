eb63b7093bea555ddf53d520fa7351a3891d00d3
G. Panagopoulos <panagopoulosgevrgios@gmail.com>
2025-04-27 13:46:59 +0300

    update

---
+++ Desktop/CF7-Angular/angular-introduction/src/app/components/person-table/person-table.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
+
+@Component({
+  selector: 'app-person-table',
+  imports: [],
+  templateUrl: './person-table.component.html',
+  styleUrl: './person-table.component.css'
+})
+export class PersonTableComponent {
+  @Input() personInput: Person | undefined;
+
+  name = "Thanasis"
+
+  person = {
+    givenName:"Thanasis",
+    surName: "Androutsos",
+    age: 20,
+    email:'thanasis@aueb.gr'
+  }
+}
